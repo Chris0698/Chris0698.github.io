b6fa0e9bee4359e1259f013fe31201db42da5fe7
chris0698 <chris.aston98@outlook.com>
2021-06-14 21:15:27 +0100

    More Sass covertion

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,41 +1,11 @@
-
 import React, { useState, useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet';
 import styled, {css} from "styled-components"
 import {navLinks} from "../config"
 import { Link } from "gatsby";
-import Styled from "styled-components"
 
-const StyledMenu = styled.div`
-  display: none;
-  @media (max-width: 768px) {
-    display: block;
-  }
-`;
-
+//this $Props is stopping it being put into the .sass file
 const StyledHamburgerButton = styled.button`
-  @media screen and (max-width: 768px) {
-    position: absolute;
-    right: 5%;
-    z-index: 10;
-    //margin-right: -15px;
-    padding: 15px;
-    //border: 0;
-    background-color: transparent;
-    color: inherit;
-    text-transform: none;
-    transition-timing-function: linear;
-    transition-duration: 0.15s;
-    transition-property: opacity, filter;
-  }
-
-  .hamburger-box {
-    display: inline-block;
-    position: relative;
-    width: 30px;
-    height: 24px;
-  }
-
   .hamburger-box-inner {
     color: var(--light-navy); /*Hack to stop test being visible*/
     position: absolute;
@@ -84,61 +54,56 @@ const StyledHamburgerButton = styled.button`
 `;
 
 const StyledSideBar = styled.div`
-  // @media screen and (max-width: 768px) {
-  //   position: fixed;
-  //   height: 100%;
-  //   overflow-x: hidden;
-  //   overflow-y: hidden;
-  //   //backdrop-filter: blur(10px);
-  //   top: 0;
-  //   bottom: 0;
-  //   right: 0;
-  //   padding: 50px 10px;
-  //   width: min(75vw, 400px);
-  //   min-height: 100vh;
-  //   //min-height: 100%;
-  //   outline: 0;
-  //   background-color: var(--light-navy);
-  //   box-shadow: -10px 0px 30px -15px var(--navy-shadow);
-  //   z-index: 9;
-  //   //this causes a horizontal scroll bar on edge and chrome.
-  //   //transform: translateX(${props => (props.menuOpen ? 0 : 100)}vw);
-  //   visibility: ${props => (props.menuOpen ? 'visible' : 'hidden')};
-  //   transition: var(--transition);
+  @media screen and (max-width: 768px) {
+    position: fixed;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    //backdrop-filter: blur(10px);
+    top: 0;
+    bottom: 0;
+    right: 0;
+    padding: 50px 10px;
+    width: min(75vw, 400px);
+    min-height: 100vh;
+    //min-height: 100%;
+    outline: 0;
+    background-color: var(--light-navy);
+    box-shadow: -10px 0px 30px -15px var(--navy-shadow);
+    z-index: 9;
+    //this causes a horizontal scroll bar on edge and chrome.
+    //transform: translateX(${props => (props.menuOpen ? 0 : 100)}vw);
+    visibility: ${props => (props.menuOpen ? 'visible' : 'hidden')};
+    transition: var(--transition);
   }
-
   nav {
-    // width: 100%;
-    // flex-direction: column;
-    // color: var(--lightest-slate);
-    // font-family: var(--font-mono);
-    // text-align: center;
+    width: 100%;
+    flex-direction: column;
+    color: var(--lightest-slate);
+    font-family: var(--font-mono);
+    text-align: center;
   }
-
   ol {
-    // padding: 0;
-    // margin: 0;
-    // list-style: none;
-    // width: 100%;
-
-    // li {
-    //   margin: 50px auto 20px;
-
-    //   &:before {
-    //     display: block;
-    //     margin-bottom: 5px;
-    //     color: var(--green);
-    //     font-size: var(--fz-sm);
-    //   }
-    // }
-
-    // a {
-    //   //background-color: pink;
-    //   width: 100%;
-    //   //padding: 30px 20px 20px;
-    //   color: white;
-    //   text-decoration: none;
-    // }
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    width: 100%;
+    li {
+      margin: 50px auto 20px;
+      &:before {
+        display: block;
+        margin-bottom: 5px;
+        color: var(--green);
+        font-size: var(--fz-sm);
+      }
+    }
+    a {
+      //background-color: pink;
+      width: 100%;
+      //padding: 30px 20px 20px;
+      color: white;
+      text-decoration: none;
+    }
   }
 `;
 
@@ -167,7 +132,7 @@ const Menu = () => {
       <Helmet>
         <body className={menuOpen ? 'blur' : ''} />
       </Helmet>
-      <StyledHamburgerButton onClick={toggleMenu} menuOpen={menuOpen} ref={buttonRef}>
+      <StyledHamburgerButton className="styledHamburgerButton" onClick={toggleMenu} menuOpen={menuOpen} ref={buttonRef}>
         <div className="hamburger-box">
           <div className="hamburger-box-inner">test</div>
         </div>
